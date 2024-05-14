79fcb70c4362193767a3b678598b7d1788404653
Bosko Kulusic <kulusicbosko@gmail.com>
2024-05-14 17:53:06 +0200

    if ... else ... then statements and logical expressions

--- hsParser.hs
+++ hsParser.hs
@@ -6,7 +6,7 @@ import Text.ParserCombinators.Parsec.Token as Token
 data ArithmeticExpr = Var String
            | IntConst Integer
            | Negative ArithmeticExpr
-           | ABinary ABinOp ArithmeticExpr ArithmeticExpr
+           | ArithmeticBinary ABinOp ArithmeticExpr ArithmeticExpr
            | FunctionCall String String
               deriving (Show)
 
@@ -16,25 +16,36 @@ data ABinOp = Add
             | Divide
                 deriving (Show)
 
-data Stmt = Sequence [Stmt]
-          | Assign String ArithmeticExpr Stmt
-          | LetIn Stmt Stmt
-          | TypeDeclaration String Type
-          | NoWhere
-            deriving (Show)
+data LogicalExpr = BoolConst Bool
+                | Not LogicalExpr
+                | LogicalBinary LogicalBinOp LogicalExpr LogicalExpr
+                deriving (Show)
+
+data LogicalBinOp = And 
+                  | Or 
+                  deriving (Show)
 
 data Type = RegularType String
             | ListType String
             | FunctionType [Type]
               deriving (Show)
+              
+data Stmt = Sequence [Stmt]
+          | Assign String ArithmeticExpr Stmt
+          | LetIn Stmt Stmt
+          | TypeDeclaration String Type
+          | If LogicalExpr Stmt Stmt
+          | NoWhere
+            deriving (Show)
+
 
 acceptableTypes :: [String]
 acceptableTypes = ["Integer", "String", "Bool"]
 
 languageDef = emptyDef  { Token.identStart      = letter
                         , Token.identLetter     = alphaNum
-                        , Token.reservedOpNames = ["+", "-", "*", "/", "=", "<", ">", "::" ]
-                        , Token.reservedNames = ["let", "in", "where"]
+                        , Token.reservedOpNames = ["+", "-", "*", "/", "=", "<", ">", "::", "->" ]
+                        , Token.reservedNames = ["let", "in", "where", "True", "False", "if", "then", "else"]
                         }
 
 lexer = Token.makeTokenParser languageDef
@@ -64,7 +75,7 @@ sequenceOfStmt =
 
 subStatement :: Parser Stmt
 subStatement = do
-  try typeStmt <|> letInStmt <|> assignStmt
+  try typeStmt <|> letInStmt <|> assignStmt <|> ifStmt
 
 embeddedStmt =
   do list <- (subStatement <* spaces) `sepBy1` semiParser
@@ -111,6 +122,7 @@ typeStmt =
      typeName <- try parseFunctionType <|> parseRegularType <|> parseListType
      return $ TypeDeclaration var typeName
 
+--popravi ovo
 embeddedFunctionCallArgs =
   do (identifierParser <* spaces) `sepBy1` semiParser
 
@@ -118,6 +130,33 @@ functionCall :: Parser ArithmeticExpr
 functionCall =
   do f1  <- identifierParser
      FunctionCall f1 <$> identifierParser
+
+ifStmt :: Parser Stmt
+ifStmt =
+  do reservedParser "if"
+     cond  <- logicalExpression
+     mThen <- optionMaybe (try (reservedParser "then"))
+     stmt1 <- case mThen of
+        Just _ -> embeddedStmt
+        Nothing -> fail "missing then clause"
+     mElse <- optionMaybe (try (reservedParser "else"))
+     stmt2 <- case mElse of
+        Just _ -> embeddedStmt
+        Nothing -> fail "missing else clause"
+     return $ If cond stmt1 stmt2
+
+logicalExpression :: Parser LogicalExpr
+logicalExpression = buildExpressionParser bOperators bTerm
+
+bOperators = [ [Infix  (reservedOpParser "and" >> return (LogicalBinary And     )) AssocLeft,
+                Infix  (reservedOpParser "or"  >> return (LogicalBinary Or      )) AssocLeft]
+             ]
+
+bTerm =  parensParser logicalExpression
+     <|> (reservedParser "True"  >> return (BoolConst True ))
+     <|> (reservedParser "False" >> return (BoolConst False))
+
+
 
 assignStmt :: Parser Stmt
 assignStmt =
@@ -134,10 +173,10 @@ aExpression :: Parser ArithmeticExpr
 aExpression = buildExpressionParser aOperators aTerm
 
 aOperators = [ [Prefix (reservedOpParser "-"   >> return Negative)          ]
-             , [Infix  (reservedOpParser "*"   >> return (ABinary Multiply)) AssocLeft,
-                Infix  (reservedOpParser "/"   >> return (ABinary Divide  )) AssocLeft,
-                Infix  (reservedOpParser "+"   >> return (ABinary Add     )) AssocLeft,
-                Infix  (reservedOpParser "-"   >> return (ABinary Subtract)) AssocLeft]
+             , [Infix  (reservedOpParser "*"   >> return (ArithmeticBinary Multiply)) AssocLeft,
+                Infix  (reservedOpParser "/"   >> return (ArithmeticBinary Divide  )) AssocLeft,
+                Infix  (reservedOpParser "+"   >> return (ArithmeticBinary Add     )) AssocLeft,
+                Infix  (reservedOpParser "-"   >> return (ArithmeticBinary Subtract)) AssocLeft]
               ]
 
 aTerm =  parensParser aExpression
